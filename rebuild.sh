58cf5c420e294241a982b18e09071631179c6450
kazma <kazma233@outlook.com>
2024-03-20 01:09:11 +0800

    feat: add rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
+# 进程名称
+PROCESS_NAME="backup-go"
+
+# 日志文件路径
+LOG_FILE="$PROCESS_NAME.log"
+
+# 拉取最新代码
 echo "Pull code change"
 git pull
 
-# 日志文件路径
-LOG_FILE="backup-go.log"
-
-# 查找 backup-go 进程 ID
-PID=$(lsof -t -c backup-go)
+# 查找进程 ID
+PID=$(lsof -t -c "$PROCESS_NAME")
 
 # 如果进程存在
 if [ -n "$PID" ]; then
   # 杀死进程
   kill -9 $PID
-  echo "Killed backup-go process with PID $PID"
+  echo "Killed $PROCESS_NAME process with PID $PID"
 else
-  echo "No backup-go process found"
+  echo "No $PROCESS_NAME process found"
 fi
 
 # 编译新的二进制文件
-go build -o backup-go
-echo "Compiled new backup-go binary"
+go build -o "$PROCESS_NAME"
+echo "Compiled new $PROCESS_NAME binary"
+
+# 删除旧的日志文件(如果存在)
+#rm -f "$LOG_FILE"
 
 # 启动新的进程,将输出重定向到日志文件
-nohup ./backup-go >> "$LOG_FILE" 2>&1 &
-echo "Started new backup-go process. Logs will be written to $LOG_FILE"
+nohup "./$PROCESS_NAME" >> "$LOG_FILE" 2>&1 &
+echo "Started new $PROCESS_NAME process. Logs will be written to $LOG_FILE"
